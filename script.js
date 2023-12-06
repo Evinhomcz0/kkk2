1bb59bd9d4683d1c1d16c31959b82d4ee84c5423
Everson Ferreira santos <138812512+Evinhomcz0@users.noreply.github.com>
2023-12-06 10:52:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,146 @@
+function login() {
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+     
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'url_do_servidor', true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+     
+    xhr.onreadystatechange = function() {
+       if (xhr.readyState === 4 && xhr.status === 200) {
+         window.location.href = 'url_da_pagina_de_destino';
+       } else if (xhr.readyState === 4 && xhr.status !== 200) {
+         alert('E-mail ou senha incorretos. Tente novamente.');
+       }
+    };
+     
+    var data = JSON.stringify({
+       email: email,
+       password: password
+    });
+     
+    xhr.send(data);
+   }
+
+   function validaFormulario() {
+    
+    const nome = document.getElementById("nome").value;
+    const email = document.querySelector("#email").value;
+    const senha = document.getElementById("senha").value;
+    const confirmaSenha = document.getElementById("confirmaSenha").value;
+
+    console.log(senha)
+
+    // validação de campos vazios
+    if(nome === '' || email === '' || senha === ''){
+        alert("Por favor, preencha os campos vazios!")
+        return
+    }
+
+    // Validação do campo do email
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    if(!emailRegex.test(email)){
+        alert("Digite um e-mail válido!")
+        return
+    }
+
+    // Verificar tamanho da senha
+    if(senha.length < 8 ){
+        alert("A senha nao pode ser menor que '8' caracteres")
+        return
+        alert("Cadastro foi realizado com sucesso!")
+    }
+    
+    alert("Cadastro foi realizado com sucesso!")
+}
+
+function darkMode () 
+    const pagina = document.body
+    
+    pagina.classList.toggle("dark-mode")
+
+    const botao = document.querySelector("D")
+
+    const isDarkMode = document.body.classList.contains("dark-mode");
+
+    if (isDarkMode == true) {
+        D.textContent = "Modo Claro"
+    }else{
+        D.textContent = "Modo Escuro"
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
